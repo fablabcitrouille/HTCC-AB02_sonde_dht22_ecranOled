b328b72792a4f3d60a4f8fa062b625794132902f
patrick <patrickdcp77@gmail.com>
2025-01-13 00:54:40 +0100

    Mise à jour du README.md

--- src/V02_htcc_dht22.cpp
+++ src/V02_htcc_dht22.cpp
@@ -8,11 +8,12 @@
  * 3. Création de la trame de données : Les valeurs de température et d'humidité sont converties en hexadécimal et concaténées pour former la trame de données.
  * 4. Envoi de la trame de données : La trame de données est envoyée via la communication série et imprimée pour le débogage.
  * 5. Affichage des données : Les valeurs de température et d'humidité sont affichées sur un écran OLED et sur la sortie série terminal.
- * 6. Gestion de l'alimentation : La broche VEXT est utilisée pour activer et désactiver l'alimentation du capteur DHT22 afin de réduire la consommation d'énergie.
- * 7. La trame préparée pourra être ensuite envoyée sur le serveur LORAWAN TTN et fera l'objet d'un autre code.
- * 8. La librairie du HTCC-AB02 permet de déclarer Vext sans spésifier le GPIO.
- * 9. La mesure température et humidité, laisser un délai de 1 seconde entre les deux mesures.
- * 
+ * 6. Affichage du nom du fichier : Le nom du fichier source est affiché sur l'écran OLED au démarrage.
+ * 7. Gestion de l'alimentation : La broche VEXT est utilisée pour activer et désactiver l'alimentation du capteur DHT22 afin de réduire la consommation d'énergie.
+ * 8. La trame préparée pourra être ensuite envoyée sur le serveur LORAWAN TTN et fera l'objet d'un autre code.
+ * 9. La librairie du HTCC-AB02 permet de déclarer Vext sans spécifier le GPIO.
+ * 10. Pour la mesure de température et d'humidité, laisser un délai de 1 seconde entre les deux mesures.
+ * 11. Dans le futur, les données seront envoyées sur The Things Network (TTN) via le protocole LoRaWAN pour une communication longue portée et basse consommation.
  * 
  * Bibliothèques utilisées :
  * - DHT sensor library : Pour lire les mesures du capteur DHT22.
